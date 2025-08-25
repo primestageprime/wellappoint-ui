924e32dff6aef235b751a297d9ea4224ba69bf12
Peter Stradinger <peter.stradinger@primestagetechnology.com>
2025-08-25 16:58:47 -0700

    WIP Better google config handling

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import solid from 'vite-plugin-solid';
 export default defineConfig({
   plugins: [solid()],
   server: {
-    port: 3001,
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
